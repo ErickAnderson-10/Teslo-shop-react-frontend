de82d1dd2c71db11a71342e461eac7b8ae2a2e2a
Erick Anderson <302240859+ErickAnderson-10@users.noreply.github.com>
2026-08-12 16:02:00 -0500

    remove createBrowserRouter

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,4 @@
 import { lazy } from "react";
-import { createBrowserRouter, createHashRouter, Navigate } from "react-router";
 
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
@@ -16,6 +15,7 @@ import {
   AdminRoute,
   NotAuthenticatedRoute,
 } from "./components/routes/ProtectedRoutes";
+import { createHashRouter } from "react-router";
 
 const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
 const AdminLayout = lazy(() => import("./admin/layouts/AdminLayout"));
